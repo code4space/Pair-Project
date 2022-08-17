ba2b0e6ba32b142e20f7d61a4ba8104e412d3ea0
isoletslicer <ardogible@gmail.com>
2022-08-17 21:48:56 +0700

    redirect pisah seller buyer done plus logout

--- routes/route.js
+++ routes/route.js
@@ -1,3 +1,4 @@
+const { Router } = require('express')
 const Controller = require('../controllers/controller')
 
 const routes = require('express').Router()
@@ -12,33 +13,53 @@ routes.post('/login', Controller.loginMethod)
 routes.get('/register', Controller.showRegister)
 routes.post('/register', Controller.registerMethod)
 
+routes.get('/logout', Controller.logoutMethod)
+
 // req session utk is loggin :
 
-/*
-router.use(function (req, res, next) {
-  if (!req.session.userId) {
+
+routes.use(function (req, res, next) {
+  if (!req.session.username) {
     const error = 'Please Login First'
-    res.redirect(`/login?error=${error}`)
+    res.redirect(`/login?err=${error}`)
   } else {
     next()
   }
 })
-*/
 
+// // profile dulu
+// routes.get('/profile/:username', Controller.showUserProfile)
+// routes.get('/profile/:username/add', Controller.showUserProfileAddForm)
+// routes.post('/profile/:username/add', Controller.addUserProfileMethod)
+// routes.get('/profile/:username/edit', Controller.showUserProfileEditForm)
+// routes.post('/profile/:username/edit', Controller.editUserProfileMethod)
+// routes.get('/profile/:username/delete', Controller.showUserProfileEditForm)
+
+// endpoint buyer
+routes.get('/services/buyer/:username', Controller.showBuyerPage)
+
+//TODO : IMPLEMENT BUY UTK BUYER.
+//! TAPI PRIORITASIN UTK EDIT ADD DELETE KHUSUS SELLER
+
+routes.get('/services/seller/:username', Controller.showSellerPage)
 
 // req session utk privilege seller :
-/*
-router.use(function(req,res,next){
-  if(req.session.userId && req.session.role !== 'seller') {
-    const error = 'You have to be a seller'
-    res.redirect(`/login?error=${error}`)
+
+routes.use(function (req, res, next) {
+  if (req.session.username && req.session.role !== 'seller') {
+    // const error = 'You have to be a seller'
+    // res.redirect(`/login?err=${error}`)
+    // res redirect ke endpoint buyer
+    // kalo dia buyer, maka redirect ke buyer page
+    res.redirect('/services/buyer/' + req.session.username)
   }
   else {
+    // kalo dia seller, maka redirect ke seller page. lanjut aja
+    res.redirect('/services/seller/' + req.session.username)
     next()
   }
 })
 
-*/
 
 
 routes.get('/services', Controller.showAllService)
@@ -46,21 +67,14 @@ routes.get('/services', Controller.showAllService)
 
 // routes.get('/services/:id/delete', Controller.deleteUserMethod)
 
+// TODO: PENTING !
+//!  PRIORITASIN UTK EDIT ADD DELETE KHUSUS SELLER
 
-// // profile dulu
-// routes.get('/profile/:UserId', Controller.showUserProfile)
-// routes.get('/profile/:UserId/add', Controller.showUserProfileAddForm)
-// routes.post('/profile/:UserId/add', Controller.addUserProfileMethod)
-// routes.get('/profile/:UserId/edit', Controller.showUserProfileEditForm)
-// routes.post('/profile/:UserId/edit', Controller.editUserProfileMethod)
-// routes.get('/profile/:UserId/delete', Controller.showUserProfileEditForm)
-
-routes.get('/services/:UserId', Controller.showServicesById)
-routes.get('/services/:UserId/add', Controller.showServicesAddForm)
-routes.post('/services/:UserId/add', Controller.addServicesMethod)
-routes.get('/services/:UserId/edit', Controller.showServicesEditForm)
-routes.post('/services/:UserId/edit', Controller.editServicesMethod)
-routes.get('/services/:UserId/delete', Controller.deleteServices)
+routes.get('/services/seller/:username/add', Controller.showServicesSellerAddForm)
+routes.post('/services/seller/:username/add', Controller.addServicesSellerMethod)
+routes.get('/services/seller/:username/edit', Controller.showServicesSellerEditForm)
+routes.post('/services/seller/:username/edit', Controller.editServicesSellerMethod)
+routes.get('/services/seller/:username/delete', Controller.deleteSellerServices)
 
 // routes.use(function (req, res, next) {
 //   console.log('Time: ', Date.now(), 'Halo, mecahin next utk login/notlogin')
