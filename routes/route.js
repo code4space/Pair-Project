b82c58df0acaa74870dd09ba1524d99d4ff2c705
isoletslicer <ardogible@gmail.com>
2022-08-17 18:02:53 +0700

    bikin seadanya dulu skeleton dll nya

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const Controller = require('../controllers/controller')
 
 const routes = require('express').Router()
 
-routes.get('/', Controller.showHome)
+routes.get('/', Controller.showLandingPage)
 
 // bikin coba tes route all 
 
@@ -13,6 +13,30 @@ routes.get('/register', Controller.showRegister)
 routes.post('/register', Controller.registerMethod)
 
 routes.get('/services', Controller.showAllService)
+
+
+// routes.get('/services/:id/delete', Controller.deleteUserMethod)
+
+
+// // profile dulu
+// routes.get('/profile/:UserId', Controller.showUserProfile)
+// routes.get('/profile/:UserId/add', Controller.showUserProfileAddForm)
+// routes.post('/profile/:UserId/add', Controller.addUserProfileMethod)
+// routes.get('/profile/:UserId/edit', Controller.showUserProfileEditForm)
+// routes.post('/profile/:UserId/edit', Controller.editUserProfileMethod)
+// routes.get('/profile/:UserId/delete', Controller.showUserProfileEditForm)
+
+routes.get('/services/:UserId', Controller.showServicesById)
+routes.get('/services/:UserId/add', Controller.showServicesAddForm)
+routes.post('/services/:UserId/add', Controller.addServicesMethod)
+routes.get('/services/:UserId/edit', Controller.showServicesEditForm)
+routes.post('/services/:UserId/edit', Controller.editServicesMethod)
+routes.get('/services/:UserId/delete', Controller.deleteServices)
+
+// routes.use(function (req, res, next) {
+//   console.log('Time: ', Date.now(), 'Halo, mecahin next utk login/notlogin')
+//   next()
+// })
 
 
 module.exports = routes
